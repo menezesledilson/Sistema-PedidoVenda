0ef3586ae3f91872618b4f8a5892a4e30a7ba709
menezesledilson <ledilsonmenezes@gmail.com>
2023-11-28 15:53:51 -0300

    Criacao model

---
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/model/Cliente.java
@@ -0,0 +1,86 @@
+package com.algaworks.pedidovenda.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String nome;
+	private String email;
+	private String documentoReceitaFederal;
+	private TipoPessoa tipo;
+	
+	private List<Endereco> endereco = new ArrayList<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDocumentoReceitaFederal() {
+		return documentoReceitaFederal;
+	}
+
+	public void setDocumentoReceitaFederal(String documentoReceitaFederal) {
+		this.documentoReceitaFederal = documentoReceitaFederal;
+	}
+
+			
+	public TipoPessoa getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoPessoa tipo) {
+		this.tipo = tipo;
+	}
+
+	public List<Endereco> getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(List<Endereco> endereco) {
+		this.endereco = endereco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/model/Endereco.java
@@ -0,0 +1,102 @@
+package com.algaworks.pedidovenda.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Endereco implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String Logradouro;
+	private String numero;
+	private String complemento;
+	private String cidade;
+	private String uf;
+	private String cep;
+	
+	private Cliente cliente;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogradouro() {
+		return Logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		Logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
